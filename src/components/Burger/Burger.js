64f92c33313467ce893813692df081d04129e400
La Ode Safar <laodesafar94@gmail.com>
2020-03-13 12:46:35 +0700

    Output Bumbu dinamis

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from './Burger.module.css';
 import BumbuBurger from './BumbuBurger/BumbuBurger';
 
 const burger = props => {
+    const transformasiBumbu = Object.keys(props.bumbu)
+        .map(buKey => {
+            return [...Array( props.bumbu[buKey])].map((_, i) => {
+                return <BumbuBurger key={buKey + i} type={buKey} />;
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BumbuBurger type="bread-top"/>
-            <BumbuBurger type="cheese"/>
-            <BumbuBurger type="meat"/>
+            {transformasiBumbu}
             <BumbuBurger type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,18 @@ import Aux from "../../hoc/Bantu";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    bumbu: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger bumbu={this.state.bumbu} />
         <div>Build Controls</div>
       </Aux>
     );
