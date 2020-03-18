a404afef5d214a0acba977a3f6cc3c286325d995
La Ode Safar <laodesafar94@gmail.com>
2020-03-18 10:54:49 +0700

    menambahkan Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logoBurger from '../../assets/images/logo.png';
+import classes from './Logo.module.css';
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={logoBurger} alt="Logo Burger" />
+    </div>
+
+);
+
+export default logo;
