acb61941e1d2654c7acf19460e6ccaa91f7b6b49
La Ode Safar <laodesafar94@gmail.com>
2020-03-18 10:24:46 +0700

    menambahkan Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,12 @@ import React from "react";
 
 import Aux from "../../hoc/Bantu";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => (
   <Aux>
-    <div> Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
